4ae7a2f58f8a6732e3468de5cd89f658ce5ae2a7
NishantTheProgrammer <knishant113@gmail.com>
2024-07-29 23:02:13 +0530

    added runner shell script

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 package src;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hey");
+        QueryResolver qr = new QueryResolver("test");
+
+        qr.prompt();
     }
 }
