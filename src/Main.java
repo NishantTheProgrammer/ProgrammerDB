cf1159544b6c15f747ab4b9f9d0c0937354048bc
NishantTheProgrammer <knishant113@gmail.com>
2024-07-30 10:21:35 +0530

    added switch case for query

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hey");
-        QueryResolver qr = new QueryResolver("test");
-
+        QueryResolver qr = new QueryResolver("school");
         qr.prompt();
     }
 }
--- src/QueryResolver.java
+++ src/QueryResolver.java
@@ -1,15 +1,50 @@
 package src;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class QueryResolver {
+    String databaseName;
     QueryResolver(String databaseName) {
-        System.out.print("databaseName: " + databaseName);
+        this.databaseName = databaseName;
     }
 
     public int excecute(String query) {
 
+        String[] tokens = query.split("\\s+");
+
+        
         System.out.println(ConsoleColors.BLUE + "EXCECUTING QUERY: " + ConsoleColors.RESET + query);
+        switch (tokens[0].toUpperCase()) {
+            case "SELECT": return this.select(tokens);
+            case "INSERT": return this.insert(tokens);
+            case "DELETE": return this.delete(tokens);
+            case "UPDATE": return this.update(tokens);
+        
+            default:
+                System.out.print(ConsoleColors.RED  + "INVALID COMMAND" + ConsoleColors.RESET);
+                break;
+        }
+        return 1;
+    }
+
+    public int select(String[] tokens) {
+        System.out.println("This command will fetch some data");
+        return 1;
+
+    }
+    public int insert(String[] tokens) {
+        System.out.println("This command will store some data");
+        return 1;
+
+    }
+    public int update(String[] tokens) {
+        System.out.println("This command will update some data");
+        return 1;
+
+    }
+    public int delete(String[] tokens) {
+        System.out.println("This command will remove some data");
         return 1;
     }
 
@@ -19,9 +54,12 @@ public class QueryResolver {
         Scanner sc = new Scanner(System.in);
         String command; 
         while(true) {
-            System.out.print(ConsoleColors.GREEN_BOLD + "ProgrammerDB> " + ConsoleColors.RESET);
+            System.out.print(ConsoleColors.GREEN_BOLD + "ProgrammerDB(" + this.databaseName +")>" + ConsoleColors.RESET);
             command = sc.nextLine();
-            if(command.equalsIgnoreCase("exit")) break;
+            if(command.equalsIgnoreCase("exit")) {
+                System.out.println("BYE");
+                break;
+            }
 
             this.excecute(command);
         }
