f0c46cc5cbba86cac199927c04ae52705639ddc1
NishantTheProgrammer <knishant113@gmail.com>
2024-07-30 13:05:02 +0530

    added Column.java

---
+++ src/Column.java
@@ -0,0 +1,29 @@
+package src;
+public class Column {
+    String name;
+    String type;
+    Boolean nullable = true;
+    Boolean primary = false;
+    Boolean unique = false;
+    Object defaultValue;
+    String comment;
+
+    // Constructor with defaults
+    public Column(String name, String type, Object defaultValue, String comment) {
+        this.name = name;
+        this.type = type;
+        this.defaultValue = defaultValue;
+        this.comment = comment;
+    }
+
+    // Constructor with all parameters
+    public Column(String name, String type, Boolean nullable, Boolean primary, Boolean unique, Object defaultValue, String comment) {
+        this.name = name;
+        this.type = type;
+        this.nullable = nullable;
+        this.primary = primary;
+        this.unique = unique;
+        this.defaultValue = defaultValue;
+        this.comment = comment;
+    }
+}
